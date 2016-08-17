749d99bcd79bfa9adadc6e3627b82a9855af7aa0
Nehal Soni <nehal.soni@britishgas.co.uk>
2016-08-18 00:07:13 +0100

    port

--- tools/distServer.js
+++ tools/distServer.js
@@ -11,9 +11,9 @@ console.log(chalkProcessing('Opening production build...'));
 
 // Run Browsersync
 browserSync({
-  port: 3000,
+  port: 8080,
   ui: {
-    port: 3001
+    port: 8080
   },
   server: {
     baseDir: 'dist'
