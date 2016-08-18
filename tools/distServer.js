e365feb6afb2647aafc226f7461199ddb13fa798
Nehal Soni <nehal.soni@britishgas.co.uk>
2016-08-18 23:05:34 +0100

    production dist server port

--- tools/distServer.js
+++ tools/distServer.js
@@ -1,5 +1,13 @@
-var express = require("express");
-var app = express();
-app.get('/', function(req, res) {
-  res.sendFile(path.join(__dirname + '/dist/index.html'));
+let express = require('express');
+let path = require('path');
+let app = express();
+
+let isProduction = process.env.NODE_ENV === 'production';
+let port = isProduction ? process.env.PORT : 3000;
+let publicPath = path.resolve(__dirname, '../dist');
+
+app.use(express.static(publicPath));
+
+app.listen(port, function () {
+  console.log('Server running on port ' + port);
 });
