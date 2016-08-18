93d8092f954ad3408b99305784aeccd4bd29391d
Nehal Soni <nehal.soni@britishgas.co.uk>
2016-08-18 08:50:21 +0100

    modify distserver

--- tools/distServer.js
+++ tools/distServer.js
@@ -1,27 +1,5 @@
-// This file configures a web server for testing the production build
-// on your local machine.
-
-import browserSync from 'browser-sync';
-import historyApiFallback from 'connect-history-api-fallback';
-import {chalkProcessing} from './chalkConfig';
-
-/* eslint-disable no-console */
-
-console.log(chalkProcessing('Opening production build...'));
-
-// Run Browsersync
-browserSync({
-  port: 8080,
-  ui: {
-    port: 8080
-  },
-  server: {
-    baseDir: 'dist'
-  },
-
-  files: [
-    'src/*.html'
-  ],
-
-  middleware: [historyApiFallback()]
+var express = require("express");
+var app = express();
+app.get('/', function(req, res) {
+  res.sendFile(path.join(__dirname + '/dist/index.html'));
 });
